0996c7dd8584a97d2326fe0499e25bd0a59d64fc
wise65 <cent.wise65@aol.com>
2025-08-14 15:23:18 -0700

    Update server.js

--- secure_doc_page/server.js
+++ secure_doc_page/server.js
@@ -201,7 +201,8 @@ app.get('/__status', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
   logLine(`SERVER_STARTED port=${PORT}`);
 });
+
